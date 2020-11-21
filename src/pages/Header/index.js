e91ea9c9a08e468ce840735f920e271bd89b5605
Neylan-Body <neylan2011@gmail.com>
2020-11-21 15:33:36 -0300

    initial commit

---
+++ src/pages/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export default function Header(props){
+    return(
+        <header title="Meu App" id="main-header">
+            <h1>{props.title}</h1>
+        </header>
+    );
+}
